0fe6218145d66fcd62ce64880601ca7b1f6414a5
Nima Samsami <nima.samsami@outlook.com>
2021-11-17 16:06:34 +0100

    init commin nima

---
+++ main copy.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("./data/realKnownCause/ambient_temperature_system_failure.csv")
+
+print(df.info())
--- main.py
+++ main.py
@@ -1,5 +1,103 @@
-import pandas as pd
+# libraries
+#%matplotlib notebook
 
-df = pd.read_csv("./data/realKnownCause/ambient_temperature_system_failure.csv")
+import pandas as pd
+import numpy as np
+
+import matplotlib
+import seaborn
+import matplotlib.dates as md
+from matplotlib import pyplot as plt
+
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.covariance import EllipticEnvelope
+#from pyemma import msm # not available on Kaggle Kernel
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
+
+# some function for later
+
+# return Series of distance between each point and his distance with the closest centroid
+def getDistanceByPoint(data, model):
+    distance = pd.Series()
+    for i in range(0,len(data)):
+        Xa = np.array(data.loc[i])
+        Xb = model.cluster_centers_[model.labels_[i]-1]
+        distance.set_value(i, np.linalg.norm(Xa-Xb))
+    return distance
+
+# train markov model to get transition matrix
+def getTransitionMatrix (df):
+	df = np.array(df)
+	model = msm.estimate_markov_model(df, 1)
+	return model.transition_matrix
+
+def markovAnomaly(df, windows_size, threshold):
+    transition_matrix = getTransitionMatrix(df)
+    real_threshold = threshold**windows_size
+    df_anomaly = []
+    for j in range(0, len(df)):
+        if (j < windows_size):
+            df_anomaly.append(0)
+        else:
+            sequence = df[j-windows_size:j]
+            sequence = sequence.reset_index(drop=True)
+            df_anomaly.append(anomalyElement(sequence, real_threshold, transition_matrix))
+    return df_anomaly
+
+df = pd.read_csv("../input/realKnownCause/realKnownCause/ambient_temperature_system_failure.csv")
 
 print(df.info())
+
+# check the timestamp format and frequence 
+print(df['timestamp'].head(10))
+
+# check the temperature mean
+print(df['value'].mean())
+
+# change the type of timestamp column for plotting
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+# change fahrenheit to °C (temperature mean= 71 -> fahrenheit)
+df['value'] = (df['value'] - 32) * 5/9
+# plot the data
+df.plot(x='timestamp', y='value')
+
+# the hours and if it's night or day (7:00-22:00)
+df['hours'] = df['timestamp'].dt.hour
+df['daylight'] = ((df['hours'] >= 7) & (df['hours'] <= 22)).astype(int)
+
+# the day of the week (Monday=0, Sunday=6) and if it's a week end day or week day.
+df['DayOfTheWeek'] = df['timestamp'].dt.dayofweek
+df['WeekDay'] = (df['DayOfTheWeek'] < 5).astype(int)
+# An estimation of anomly population of the dataset (necessary for several algorithm)
+outliers_fraction = 0.01
+
+# time with int to plot easily
+df['time_epoch'] = (df['timestamp'].astype(np.int64)/100000000000).astype(np.int64)
+
+# creation of 4 distinct categories that seem useful (week end/day week & night/day)
+df['categories'] = df['WeekDay']*2 + df['daylight']
+
+a = df.loc[df['categories'] == 0, 'value']
+b = df.loc[df['categories'] == 1, 'value']
+c = df.loc[df['categories'] == 2, 'value']
+d = df.loc[df['categories'] == 3, 'value']
+
+fig, ax = plt.subplots()
+a_heights, a_bins = np.histogram(a)
+b_heights, b_bins = np.histogram(b, bins=a_bins)
+c_heights, c_bins = np.histogram(c, bins=a_bins)
+d_heights, d_bins = np.histogram(d, bins=a_bins)
+
+width = (a_bins[1] - a_bins[0])/6
+
+ax.bar(a_bins[:-1], a_heights*100/a.count(), width=width, facecolor='blue', label='WeekEndNight')
+ax.bar(b_bins[:-1]+width, (b_heights*100/b.count()), width=width, facecolor='green', label ='WeekEndLight')
+ax.bar(c_bins[:-1]+width*2, (c_heights*100/c.count()), width=width, facecolor='red', label ='WeekDayNight')
+ax.bar(d_bins[:-1]+width*3, (d_heights*100/d.count()), width=width, facecolor='black', label ='WeekDayLight')
+
+plt.legend()
+plt.show()
+
